9a673a64df0edcefd3df41ae274e86c850917371
Apprentice <apprentice@dbc-chicago031.local>
2013-11-01 14:09:13 -0500

    Removed unneeded comments from models/deck.rb

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -3,28 +3,4 @@ class Deck < ActiveRecord::Base
   has_many :cards
   has_many :rounds
   belongs_to :user
-
-  # def get_random_unsolved_card
-  #   # unsolved_cards = []
-
-  #   # unsolved_cards = Cards.where()
-
-  #   # wrong_guesses = Guess.where("correct = ?", false)
-
-  #   # wrong_guesses.each do |guess|
-  #   #   card_in_this_guess = guess.cards.first
-  #   #   unsolved_cards << guess.cards.first
-  #   # end
-
-  #   # unsolved_cards_in_this_deck = unsolved_cards.where("")
-
-  #   # self.cards.each do |card|
-  #   #   Guess.where
-
-
-  #   # end
-
-
-
-  # end
 end
