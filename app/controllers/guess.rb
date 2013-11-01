a388114742efa3f562f1f7a4f2517e6c273319f5
Apprentice <apprentice@dbc-chicago031.local>
2013-11-01 15:44:00 -0500

    Added guess.rb controller/routes file (it calls some methods in Round that are NYI)

---
+++ app/controllers/guess.rb
@@ -0,0 +1,25 @@
+get '/decks/:deck_id/cards/:card_id' do
+  @current_round = Round.where("id = ?", sessions[:round_id]).first
+  @deck = Deck.where("id = ?",  params[:deck_id]).first
+  @card = @current_round.get_random_unsolved_card #Round.get_random_unsolved_card NOT YET IMPLEMENTED
+
+  erb :display_flash_card
+end
+
+post '/decks/:deck_id/cards/:card_id' do
+  @current_round = Round.where("id = ?", session[:round_id]).first
+  @deck = Deck.where("id = ?",  params[:deck_id]).first
+  @card = Card.where("id = ?", params[:card_id]).first
+
+  user_response = params[:user_response]
+  response_validity = @card.valid_response?(user_response)
+
+  @guess = Guess.new(:round_id => @current_round.id, :card_id => @card.id, :response => user_response, :correct => response_validity)
+  @guess.save
+
+  if @current_round.get_unsolved_cards.length > 0 # Round.get_unsolved_cards NOT YET IMPLEMENTED
+    redirect to '/guesses/:guess_id'
+  else
+    redirect to '/' # NOT THE CORRECT TARGET, NOT SURE WHICH URL TO REDIRECT TO YET
+  end
+end
