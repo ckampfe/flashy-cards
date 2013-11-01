b55aa7536397d8e616abfb74d7278535447776c4
Clark Kampfe <clark.kampfe@gmail.com>
2013-11-01 18:05:01 -0500

    hybrid create-login method that checks for create button press, else login method

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,7 +1,48 @@
+# get splash page
 get '/' do
   erb :splash
 end
 
+# get login form
 get '/login' do
   erb :login
+end
+
+### brought this into main '/users' route.
+### not ideal, but it creates and logs in.
+### confer with others for better method
+
+# post '/users/new' do
+#   user = User.create(:email => params[:email],
+#                      :password => params[:password]
+#                     )
+#   session[:user_id] = user.id
+#   erb :profile
+# end
+
+# hybrid login/create
+post '/users' do
+
+  if params[:users_new] # checks if create or login attempt
+    puts "create triggered"
+    user = User.create(:email => params[:email],
+                       :password => params[:password]
+                      )
+    session[:user_id] = user.id
+    erb :profile
+
+  else # login
+    puts "login triggered"
+    user = User.where("email = ? AND password = ?", params[:email], params[:password]).first || false
+    puts user
+
+    if user # check login and password
+      session[:user_id] = user.id
+      erb :profile # success
+    else
+      erb :login # failure
+    end
+
+  end
+
 end
