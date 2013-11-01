6706995437a8eb2dbf062dc1cf44ab967658e580
Apprentice <apprentice@dbc-chicago031.local>
2013-11-01 15:42:06 -0500

    Added valid_response?(user_input) method to Card class

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,4 +3,9 @@ class Card < ActiveRecord::Base
   belongs_to :deck
   has_many :guesses
   has_many :rounds, :through => :guesses
+
+  def valid_response?(user_answer)
+    card.answer.strip == user_answer.strip
+  end
+
 end
