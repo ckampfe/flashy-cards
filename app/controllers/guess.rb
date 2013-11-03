b1dce6601c45b8628e13998e24cfa14036b3a6a5
Clark Kampfe <clark.kampfe@gmail.com>
2013-11-03 01:26:53 -0600

    ajax working, route modifications to handle/test for ajax

--- app/controllers/guess.rb
+++ app/controllers/guess.rb
@@ -1,6 +1,7 @@
 enable :sessions
+require 'json'
 
-get '/guesses/:guess_id' do 
+get '/guesses/:guess_id/:ajax' do 
   
   puts "guess id " + params[:guess_id].to_s
 
@@ -16,7 +17,18 @@ get '/guesses/:guess_id' do
     @outcome = "Incorrect"
   end
 
-  erb :answer
+  if params[:ajax] == "1" 
+    respy = { :answer  => @answer,
+              :outcome => @outcome,
+              :deck    => @deck
+             }
+
+    puts respy
+    puts respy.to_json
+    respy.to_json
+  else 
+    erb :answer
+  end
 end 
 
 get '/decks/:deck_id/draw_card' do
@@ -45,14 +57,35 @@ post '/decks/:deck_id/cards/:card_id' do
   @deck = Deck.where("id = ?",  params[:deck_id]).first
   @card = Card.where("id = ?", params[:card_id]).first
 
+  puts "got here"
+
+  if request.xhr?
+    puts "got into ajax true"
+    ajax = true
+  else
+    puts "got into ajax false"
+    ajax = false
+  end
+
+  puts params
+  puts "got just before user response"
   user_response = params[:user_response]
+  puts params[:user_response]
+  puts user_response
   response_validity = @card.valid_response?(user_response)
 
   @guess = Guess.new(:round_id => @current_round.id, :card_id => @card.id, :response => user_response, :correct => response_validity)
   @guess.save
 
+
   if @current_round.get_unsolved_cards.length > 0
-    redirect to "/guesses/#{@guess.id}"
+
+    if ajax
+      redirect to "/guesses/#{@guess.id}/1"
+    else
+      redirect to "/guesses/#{@guess.id}/0"
+    end
+
   else 
     redirect to "/rounds/#{@current_round}/statistics"
   end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,41 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  /* AJAX TO POST CARD ANSWER */
+
+  // this is the event
+  $( "#answer-form" ).on( "submit", function(event) {
+    event.preventDefault();
+    // this calls the subroutines
+    answerQuestion();
+  });
+
+  function answerQuestion() {
+    // get data from form
+    var formData = getFormData();
+    // send data to server and receive response
+    var serverSays = sendData(formData); 
+    // modify DOM / fade 
+    modifyDom(serverSays); 
+  }
+
+  function getFormData() {
+    var url = $( "#answer-form" ).attr( "action" );
+    var answer = $( "#answer-form > input" ).val();
+    console.log(url);
+    return { "url": url, "answer": answer };
+  }
+
+  function sendData(formData) {
+    var dataObject = { "user_response": formData.answer }
+    console.log(dataObject);
+    $.post(formData.url, dataObject, function( response ) {
+      console.log(response);
+      return response;
+    });
+  }
+
+  function modifyDom(serverSays) {
+    console.log("made it here!");
+    console.log(serverSays);
+  }
 });
